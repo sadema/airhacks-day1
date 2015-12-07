822d487b0ecacd9f838c54ff44015a59622be340
Adam Bien <abien@adam-bien.com>
2015-12-07 14:06:15 +0100

    @Asynchronous with futures

--- helloserver/src/main/java/com/airhacks/hello/GreetingsResource.java
+++ helloserver/src/main/java/com/airhacks/hello/GreetingsResource.java
@@ -1,5 +1,9 @@
 package com.airhacks.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.SessionContext;
@@ -25,6 +29,9 @@ public class GreetingsResource {
     @EcoMode(EcoMode.Level.SMART)
     Engine engine;
 
+    @Inject
+    HeavyLifting lift;
+
     @Resource
     SessionContext ctx;
 
@@ -35,10 +42,21 @@ public class GreetingsResource {
 
     @GET
     public void whatever(@Suspended AsyncResponse response) {
+        List<Future<String>> computations = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Future<String> future = lift.lift();
+            computations.add(future);
+        }
+        for (Future<String> computation : computations) {
+            try {
+                System.out.println(computation.get());
+            } catch (InterruptedException | ExecutionException ex) {
+
+            }
+        }
         this.engine.start();
         System.out.println("-- " + bl.getClass());
         response.resume("async hello appserver " + System.currentTimeMillis() + " " + bl.getMessage());
-        ctx.setRollbackOnly();
     }
 
 }
---
+++ helloserver/src/main/java/com/airhacks/hello/HeavyLifting.java
@@ -0,0 +1,42 @@
+package com.airhacks.hello;
+
+import java.time.LocalTime;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Startup
+@Singleton
+public class HeavyLifting {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("-- starting the singleton");
+    }
+
+    @Asynchronous
+    public Future<String> lift() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(HeavyLifting.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return new AsyncResult<>("42");
+    }
+
+    @Schedule(hour = "*", minute = "*", second = "*/2")
+    public void repetetiveWork() {
+        System.out.println("--- executing " + LocalTime.now());
+    }
+
+}
